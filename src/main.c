60e9e0476c5a7e33e8b154de315e266660af539e
Lightning Potato <lightnin@mi-t4.msk.21-school.ru>
2023-03-07 17:05:17 +0300

    parsing started

--- src/main.c
+++ src/main.c
@@ -5,9 +5,10 @@
 #include <ctype.h>
 
 
-#define OPERAND   0
-#define NUMBER 	  1
-#define ERROR     1
+#define OPERAND   	 0
+#define NUMBER 	  	 1
+#define ERROR     	 1
+#define ERROR_CH    '!'
 #define VALID_CHARACTERS "1234567890.()+-*/^modcsintaqrtlg "
 
 typedef union Value
@@ -38,7 +39,7 @@ void* allocate(int flag) {
 	return flag ? calloc(32, 1) : calloc(16, 1);
 }
 
-void* push(void *head, void* value, int flag_head) {
+void* push(void* head, void* value, int flag_head) {
 	void *current = NULL;
 	if (flag_head == 0) {
 		current = (t_node_oper*)allocate(OPERAND);
@@ -56,7 +57,11 @@ void* allocate(int flag_head);
 void* push(void *head, void* c, int flag_head);
 int parser(char *str);
 double get_number(char **pointer);
-char get_character(char **str);
+char get_character(char **str, int *err);
+char get_function(char **str);
+// int check_correct_func(char **str);
+char get_triginometric_func(char **str);
+void remove_spases(char *str, char **result);
 
 int main(int argc, char const *argv[])
 {
@@ -64,8 +69,8 @@ int main(int argc, char const *argv[])
 	char *p = a;
 	t_node_value* head_value = NULL;
 	t_node_oper* head_oper = NULL;
-	parser("       12345 +     1234 * 5");
-
+	// printf("%d\n", parser("-tn(-5)"));
+	parser("-tn(-5)");
 	// while (head_value != NULL) 
 	// {
 		// printf("%d\n", head_value->val);
@@ -74,15 +79,25 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
+void remove_spases(char *str, char **result) {
+	int i = 0;
+	while (*str != '\0') {
+		if (*str != ' ') {
+			*((*result)+i) = *str;
+			i++;
+		}
+		str++;
+	}
+}
 
 int parser(char *str) {
 	char *result = (char *)calloc(1000, sizeof(char));
 	char *str_buf = (char *)calloc(1000, sizeof(char));
+	char *p = str_buf;
 	int num_flag_err = 0;
-	int i = 0;
-	while (*str++) {
-		if (*str != ' ') str_buf[i++] = *str;
-	}
+	int char_flag_err = 0;
+	remove_spases(str, &str_buf);
+	printf("%s\n", str_buf);
 	if (*str_buf == '+' || *str_buf == '-') {
 		result[0] = '0';
 	}
@@ -95,24 +110,82 @@ int parser(char *str) {
 			sprintf(buff, "%.13f", num);
 			strcat(result, buff);
 			num_flag_err = 1;
+			// printf("%s\n", result);
+			// printf("%s\n", str_buf);
 		} else {
-			if (*str_buf == '(' && (*(str_buf+1) == '+' || *(str_buf+1) == '-') && isdigit(*(str_buf+2))) {
-				result[strlen(result)] = '(';
-				result[strlen(result)+1] = '0';
+			if (*str_buf == '(' && (*(str_buf+1) == '+' || *(str_buf+1) == '-') && isdigit(*(str_buf+2))) { //если скобка а за ней унарный оператор(обработка краевой ситуации)
+				strcat(result, "(0");
 				str_buf++;
 				continue;
 			}
-			if (check_binary_operator())
-			result[strlen(result)] = get_character(&str_buf);
+			// printf("%c\n", get_character(&str_buf, &char_flag_err));
+			result[strlen(result)] = get_character(&str_buf, &char_flag_err);
+			if (char_flag_err == 1) return ERROR;
 			num_flag_err = 0;
 		}
 	}
-	free(str_buf);	
+	printf("%s\n", result);
+	free(p);
+	return 0;
 }
 
-char get_character(char **str) {
-
+char get_character(char **str, int *err) {
+	// printf("aboba %s\n", *str);
+	char res = 0;
+	if (strchr("+-*/^", **str) != NULL && strchr("+-*/^m", *(*str+1)) == NULL) { //если текущий символ оператор +-*/^ и следущий не оператор +-*/^
+		char tmp = **str;
+		(*str)++;
+		return tmp;
+	} else if (strchr("cstal", **str) != NULL) { //если текущий символ начало функции
+		res = get_function(str);
+		if (res == '!') *err = 1; //некорректный ввод
+	} else if (**str == '(' || **str == ')') { //если текущий символ скобка ()
+		char tmp = **str;
+		(*str)++;
+		return tmp;
+	} 
+	return res;
 }
+
+char get_function(char **str) {
+	char res = '!';
+	if (strchr("cst", **str) != NULL && *(*str+1) != 'q') { //если это тригонометрическая функция
+		res = get_triginometric_func(str);
+	} else if (**str == 'a') {
+		res = get_triginometric_func(str);
+	}
+	return res;
+}
+
+char get_triginometric_func(char **str) {
+	// int err = 1;
+	char res = '!';
+	char buff[5];
+	strncpy(buff, *str, 4);
+	buff[4] = '\0';
+	if (strcmp("sin(", buff) == 0) {
+		*str = *str + 3;
+		res = 's'; //возвращаем s если в строке корректный sin()
+		// err = 0;
+	} else if (strcmp("cos(", buff) == 0) {
+		*str = *str + 3;
+		res = 'c'; //возвращаем c если в строке корректный cos()
+		// err = 0;
+	} else if (strcmp("tan(", buff) == 0) {
+		*str = *str + 3;
+		res = 't'; //возвращаем t если в строке корректный tan()
+		// err = 0;
+	}
+	
+	// if (check_correct_func(str) == 0 && err == 0) {
+	// 	return res;
+	// }
+	return res;
+ }
+
+//  int check_correct_func(char **str) {
+
+//  }
 
 double get_number(char **pointer) {
 	char *buff = *pointer;
